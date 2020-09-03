82fb60703a875b10d28e6f28f561ef35e36a2ac3
Vishruth S <vs.vishruth.s@gmail.com>
2020-09-03 11:42:17 +0530

    Added CNN

---
+++ Part-7 Deep Learning/Convolutional Neural Networks/convolutional_neural_network.py
@@ -0,0 +1,69 @@
+# Convolutional Neural Network
+
+# Step 0 - Importing the libraries
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+# Step 1 - Data Preprocessing
+
+# Preprocessing the Training set
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
+
+# Preprocessing the Test set
+test_datagen = ImageDataGenerator(rescale = 1./255)
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
+
+# Step 2 - Building the CNN
+
+# Initialising the CNN
+cnn = tf.keras.models.Sequential()
+
+# Part 1 - Convolution
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[64, 64, 3]))
+
+# Part 2 - Pooling
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
+# Adding a second convolutional layer
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
+# Part 3 - Flattening
+cnn.add(tf.keras.layers.Flatten())
+
+# Part 4 - Full Connection
+cnn.add(tf.keras.layers.Dense(units=128, activation='relu'))
+
+# Part 5 - Output Layer
+cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+# Step 3 - Training the CNN
+
+# Compiling the CNN
+cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Training the CNN on the Training set and evaluating it on the Test set
+cnn.fit(x = training_set, validation_data = test_set, epochs = 25)
+
+# Step 4 - Making a single prediction
+from keras.preprocessing import image
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = cnn.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+print(prediction)
